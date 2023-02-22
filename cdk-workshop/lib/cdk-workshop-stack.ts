3d04f6fee21a74046c902a10e8beb33ac26ceda4
Yen Liu <yenanliu@amazon.com>
2023-02-22 12:49:18 +0000

    update

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -3,17 +3,31 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+// https://cdkworkshop.com/20-typescript/30-hello-cdk/200-lambda.html
+
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X, // exe env
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler' // file is "hello", function is "handler"
+    }) 
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    // https://cdkworkshop.com/20-typescript/30-hello-cdk/100-cleanup.html
+    
+    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
   }
+
 }
