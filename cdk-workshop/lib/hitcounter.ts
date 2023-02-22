6ff625277a8da0c17ae146889a6184284467414e
Yen Liu <yenanliu@amazon.com>
2023-02-22 14:15:29 +0000

    update

--- cdk-workshop/lib/hitcounter.ts
+++ cdk-workshop/lib/hitcounter.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 // https://cdkworkshop.com/20-typescript/40-hit-counter/100-api.html
 
@@ -11,8 +12,27 @@ export interface HitCounterProps{
 }
 
 export class HitCounter extends Construct{
-    constructor(scope: Construct, id:String, props: HitCounterProps){
+
+    /** allows accessing the counter function */
+    public readonly handler: lambda.Function;
+
+    constructor(scope: Construct, id: string, props: HitCounterProps){
         super(scope, id);
-        // 
+        // https://cdkworkshop.com/20-typescript/40-hit-counter/300-resources.html
+
+        const table = new DynamoDB.table(this, 'Hits', { // TODO : double check this
+            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+        } )
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        })
+
     }
 }
