891daddaef976c19dbeafcb605826bac0747b428
Yen Liu <yenanliu@amazon.com>
2023-03-09 14:11:38 +0000

    add ecs-helloworld2/

---
+++ ecs-helloworld2/lib/ecs-helloworld2-stack.ts
@@ -0,0 +1,71 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import { KeyPair } from 'cdk-ec2-key-pair';
+
+// https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/ecs-service-with-logging/index.ts
+
+export class EcsHelloworld2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    })
+
+    const cluster = new ecs.Cluster(this, 'Ec2Cluster', {
+      vpc: vpc
+    });
+
+    cluster.addCapacity('MyAutoScalingGroup', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+    });
+
+    // create a task definition with CloudWatch Logs
+    const logging = new ecs.AwsLogDriver({ streamPrefix: "myapp" })
+
+    const taskDef = new ecs.Ec2TaskDefinition(this, "MyTaskDefinition");
+
+    taskDef.addContainer("AppContainer", {
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      memoryLimitMiB: 512,
+      logging,
+    })
+    
+    // Instantiate ECS Service with just cluster and image
+    new ecs.Ec2Service(this, "Ec2Service", {
+      cluster,
+      taskDefinition: taskDef,
+    });
+
+
+    const mySecurityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      description: "Allow ssh access to ec2 instances from anywhere",
+      allowAllOutbound: true,
+    });
+    mySecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "allow public ssh access"
+    );
+
+    const ec2Instance = new ec2.Instance(this, "Instance", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MEDIUM
+      ),
+      machineImage: new ec2.AmazonLinuxImage(),
+      securityGroup: mySecurityGroup,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      keyName: "yen-dev-key1",
+    });
+  }
+
+  }
---
+++ ecs-helloworld2/test/ecs-helloworld2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsHelloworld2 from '../lib/ecs-helloworld2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-helloworld2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsHelloworld2.EcsHelloworld2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
