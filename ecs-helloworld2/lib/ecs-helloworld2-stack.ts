76954158a90061406fc11a8dc7a6d14d12a1f11a
Yen Liu <yenanliu@amazon.com>
2023-03-11 17:33:57 +0000

    update

--- ecs-helloworld2/lib/ecs-helloworld2-stack.ts
+++ ecs-helloworld2/lib/ecs-helloworld2-stack.ts
@@ -4,7 +4,7 @@ import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
-import { KeyPair } from 'cdk-ec2-key-pair';
+
 
 // https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/ecs-service-with-logging/index.ts
 
@@ -12,60 +12,81 @@ export class EcsHelloworld2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //   const vpc = new ec2.Vpc(this, "MyVpc", {
+    //     maxAzs: 3 // Default is all AZs in region
+    //   })
+
+    //   const cluster = new ecs.Cluster(this, 'Ec2Cluster', {
+    //     vpc: vpc
+    //   });
+
+    //   cluster.addCapacity('MyAutoScalingGroup', {
+    //     instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+    //   });
+
+    //   // create a task definition with CloudWatch Logs
+    //   const logging = new ecs.AwsLogDriver({ streamPrefix: "myapp" })
+
+    //   const taskDef = new ecs.Ec2TaskDefinition(this, "MyTaskDefinition");
+
+    //   taskDef.addContainer("AppContainer", {
+    //     image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+    //     memoryLimitMiB: 512,
+    //     logging,
+    //   })
+
+    //   // Instantiate ECS Service with just cluster and image
+    //   new ecs.Ec2Service(this, "Ec2Service", {
+    //     cluster,
+    //     taskDefinition: taskDef,
+    //   });
+
+
+    //   const mySecurityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+    //     vpc,
+    //     description: "Allow ssh access to ec2 instances from anywhere",
+    //     allowAllOutbound: true,
+    //   });
+    //   mySecurityGroup.addIngressRule(
+    //     ec2.Peer.anyIpv4(),
+    //     ec2.Port.tcp(22),
+    //     "allow public ssh access"
+    //   );
+
+    //   const ec2Instance = new ec2.Instance(this, "Instance", {
+    //     vpc,
+    //     instanceType: ec2.InstanceType.of(
+    //       ec2.InstanceClass.T2,
+    //       ec2.InstanceSize.MEDIUM
+    //     ),
+    //     machineImage: new ec2.AmazonLinuxImage(),
+    //     securityGroup: mySecurityGroup,
+    //     vpcSubnets: {
+    //       subnetType: ec2.SubnetType.PUBLIC,
+    //     },
+    //     keyName: "yen-dev-key1",
+    //   });
+
     const vpc = new ec2.Vpc(this, "MyVpc", {
-      maxAzs: 3 // Default is all AZs in region
+      //maxAzs: 3 // Default is all AZs in region
     })
 
-    const cluster = new ecs.Cluster(this, 'Ec2Cluster', {
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
       vpc: vpc
-    });
-
-    cluster.addCapacity('MyAutoScalingGroup', {
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
-    });
-
-    // create a task definition with CloudWatch Logs
-    const logging = new ecs.AwsLogDriver({ streamPrefix: "myapp" })
-
-    const taskDef = new ecs.Ec2TaskDefinition(this, "MyTaskDefinition");
-
-    taskDef.addContainer("AppContainer", {
-      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-      memoryLimitMiB: 512,
-      logging,
     })
-    
-    // Instantiate ECS Service with just cluster and image
-    new ecs.Ec2Service(this, "Ec2Service", {
-      cluster,
-      taskDefinition: taskDef,
-    });
 
-
-    const mySecurityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
-      vpc,
-      description: "Allow ssh access to ec2 instances from anywhere",
-      allowAllOutbound: true,
-    });
-    mySecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      "allow public ssh access"
-    );
-
-    const ec2Instance = new ec2.Instance(this, "Instance", {
-      vpc,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MEDIUM
-      ),
-      machineImage: new ec2.AmazonLinuxImage(),
-      securityGroup: mySecurityGroup,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      keyName: "yen-dev-key1",
-    });
-  }
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // necessary
+      cpu: 1024, // default : 256
+      desiredCount: 1, // default : 1
+      //taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("metabase/metabase") },
+      memoryLimitMiB: 1024 * 3,
+      publicLoadBalancer: true
+    })
 
   }
+
+}
