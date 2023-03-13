5dc0ebcea50d2a9eb7e3dac9689def49aaeb0d0d
Yen Liu <yenanliu@amazon.com>
2023-03-13 11:39:50 +0000

    update

--- ecs-helloworld2/lib/ecs-helloworld2-stack.ts
+++ ecs-helloworld2/lib/ecs-helloworld2-stack.ts
@@ -4,7 +4,9 @@ import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
-
+import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager'
+import * as route53 from 'aws-cdk-lib/aws-route53'
 
 // https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/ecs-service-with-logging/index.ts
 
@@ -67,6 +69,17 @@ export class EcsHelloworld2Stack extends cdk.Stack {
     //     keyName: "yen-dev-key1",
     //   });
 
+ 
+    const domain =`xxx.yyy.yen.com`;
+    const zone = new route53.HostedZone(this, "HostedZone", {
+      zoneName: domain,
+    });
+    const certificate = new acm.Certificate(this, "Certificate", {
+      domainName: domain,
+      subjectAlternativeNames: [`*.${domain}`],
+      validation: acm.CertificateValidation.fromDns(zone),
+    });
+
     const vpc = new ec2.Vpc(this, "MyVpc", {
       //maxAzs: 3 // Default is all AZs in region
     })
@@ -89,17 +102,65 @@ export class EcsHelloworld2Stack extends cdk.Stack {
 
     //new ecs.PortM(234, 3242);
 
+    const taskDefinition = new ecs.FargateTaskDefinition(this, "TaskDef");
+    taskDefinition.addContainer("DefaultContainer", {
+      image: ecs.ContainerImage.fromRegistry("metabase/metabase"),
+      portMappings: [
+        { containerPort: 3000, hostPort: 3000 },
+      ],
+    });
+
     // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-      cluster: cluster, // necessary
-      cpu: 1024, // default : 256
-      desiredCount: 1, // default : 1
-      //taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("metabase/metabase") },
-      memoryLimitMiB: 1024 * 3,
-      publicLoadBalancer: true,
-      listenerPort:80
-    })
+    // new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+    //   cluster: cluster, // necessary
+    //   cpu: 1024, // default : 256
+    //   desiredCount: 1, // default : 1
+    //   //taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+    //   taskImageOptions: { image: ecs.ContainerImage.fromRegistry("metabase/metabase") },
+    //   memoryLimitMiB: 1024 * 3,
+    //   publicLoadBalancer: true,
+    //   listenerPort:80
+    // })
+
+    // const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "Service", {
+    //   cluster,
+    //   //publicLoadBalancer: true,
+    //   assignPublicIp: true,
+    //   taskDefinition,
+    // });
+
+    const service = new ecs.FargateService(this, "Service", {
+      cluster,
+      taskDefinition,
+      assignPublicIp: true,
+    });
+
+    const lb = new elb.ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc,
+      internetFacing: true,
+    });
+
+    const listener = lb.addListener("Listener", {
+      port: 443,
+      certificates: [certificate],
+    });
+
+    listener.addTargets("UI", {
+      port: 80,
+      targets: [service.loadBalancerTarget({ containerName: "DefaultContainer", containerPort: 3000 })],
+      healthCheck: {
+        healthyHttpCodes: "200-399",
+      },
+    });
+
+    
+    // service.loadBalancer.addRedirect();
+
+    // service.listener.addTargets("api", {
+    //   priority: 10,
+    //   //conditions: [elb.ListenerCondition.hostHeaders([`api.${domain}`])],
+    //   // what to do???
+    // });
 
   }
 
